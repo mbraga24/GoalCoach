5d204bf94b4782e80e6bda21be6441a5ebbfa450
github username <rdg.brg@gmail.com>
2020-09-13 15:03:47 -0400

    SignUp form

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,8 +1,47 @@
 import React from 'react';
+import userInputState from '../hooks/userInputState';
 
 const SignUp = () => {
+
+  const [ email, setEmail, resetEmail ] = userInputState("")
+  const [ password, setPassword, resetPassword ] = userInputState("")
+
+  const signUp = e => {
+    e.preventDefault()
+
+    console.log(email)
+    console.log(password)
+    resetEmail()
+    resetPassword()
+  }
+
   return (
-    <h1>SignUp</h1>
+    <div className="form-inline">
+      <h2>Sign Up</h2>
+      <div className="form-group">
+        <input 
+          className="form-control"
+          type="text"
+          placeholder="email"
+          value={email}
+          onChange={setEmail}
+        />
+        <input 
+          className="form-control"
+          type="password"
+          placeholder="password"
+          value={password}
+          onChange={setPassword}
+        />
+        <button 
+          className="btn btn-primary"
+          type="button"
+          onClick={signUp}
+          >
+          Sign Up
+        </button>
+      </div>
+    </div>
   );
 }
 
