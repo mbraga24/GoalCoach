d36529ddbeae13c7cfe98e4eed495016ecc38f3f
github username <rdg.brg@gmail.com>
2020-09-14 15:47:41 -0400

    Collect goals from firebase database and display in the console

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -1,6 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { goalRef } from '../firebase';
 
 const GoalList = () => {
+
+  useEffect(() => {
+    goalRef.on('value', snap => {
+      const goals = []
+      snap.forEach(goal => {
+        const { email, title } = goal.val()
+        goals.push({ email, title })
+      })
+      console.log("GOALS - GOAL LIST: ", goals)
+    })
+  }, [])
+
   return (
     <div>
       <h4>goal 1</h4>
